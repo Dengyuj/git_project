cf1745f32164cc348e135b47582a7a73bf74fe7e
Dengyuj <74592597+Dengyuj@users.noreply.github.com>
2020-12-01 17:41:11 +0800

    Update StudentModel.java

--- student_prc/src/student_prc/StudentModel.java
+++ student_prc/src/student_prc/StudentModel.java
@@ -1,69 +1,7 @@
-package student_prc;
-
-public class StudentModel {
-      private int id;
-      private String username;
-      private String password;
-      private int sex;
-      private String lova;
-	private String love;
-      
-      public StudentModel(int id,String username,String password,int sex,String love) {
-
-            super();
-            this.setId(id);
-            this.setUsername(username);
-            this.setPassword(password);
-            this.setSex(sex);
-            this.setLove(love);
-            
-      }
-
-	public String getLove() {
-		return love;
-	}
-
-	public void setLove(String love) {
-		this.love = love;
-	}
-
-	public String getLova() {
-		return lova;
-	}
-
-	public void setLova(String lova) {
-		this.lova = lova;
-	}
-
-	public int getSex() {
-		return sex;
-	}
-
-	public void setSex(int sex) {
-		this.sex = sex;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-}
+[branch "master"] 
+	remote = origin 
+	merge = refs/heads/master
+[remote "origin"] 
+	url = https://github.com/huangbangyi2019/git_project.git 
+	fetch = +refs/heads/*:refs/remotes/origin/* 
+	push = refs/heads/master:refs/heads/master
